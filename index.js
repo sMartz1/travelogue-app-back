76a41cce01b67631845b61ad8db63f4a2c68454f
miguelCidPaz <miguel.cid.paz@gmail.com>
2022-04-22 20:23:28 +0200

    format index

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const app = require("express")();
 const cors = require("cors");
 var bodyParser = require("body-parser");
+
 app.use(bodyParser.json());
 app.use(cors());
 require("dotenv").config();
+
 const PORT = process.env.PORT;
+
 app.use("/api", require("./routes"));
+
 app.listen(PORT,()=>{
     console.log(`Server running on PORT ${PORT}`);
 })
