e6dd8db673a4d2646bbd6b9597e6b70404944f6b
Sebastian M <ra3gan1991@gmail.com>
2022-04-29 13:52:59 +0200

    Added sequelize pg conection config

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require("express")();
 const cors = require("cors");
+const {sequelize}  = require("./models")
 var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(cors());
@@ -8,4 +9,11 @@ const PORT = process.env.PORT;
 app.use("/api", require("./routes"));
 app.listen(PORT,()=>{
     console.log(`Server running on PORT ${PORT}`);
+    try {
+        sequelize.authenticate();
+        console.log('Connection has been established successfully to PG.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+
 })
