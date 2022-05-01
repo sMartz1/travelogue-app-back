58a4a70b83c6c6fb945349c5ca5b2b2c5ea52373
miguelCidPaz <miguel.cid.paz@gmail.com>
2022-05-01 16:01:31 +0200

    connection with s3

--- config.js
+++ config.js
@@ -3,5 +3,7 @@ require("dotenv").config();
 module.exports = {
     PORT: process.env.PORT,
     MONGO_USER: process.env.MONGO_USER,
-    MONGO_PASS: process.env.MONGO_PASS
+    MONGO_PASS: process.env.MONGO_PASS,
+    S3_ID_ACCESSKEY: process.env.S3_ID_ACCESSKEY,
+    S3_SECRET_ACCESSKEY: process.env.S3_SECRET_ACCESSKEY
 }
---
+++ managers/connections/ConnectionToS3.js
@@ -0,0 +1,8 @@
+const config = require('./../../config')
+const AWS = require('aws-sdk')
+const s3 = new AWS.S3({
+    accessKeyId: config.S3_ID_ACCESSKEY,
+    secretAccessKey: config.S3_SECRET_ACCESSKEY
+})
+
+export default s3
