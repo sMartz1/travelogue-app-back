2ab5ba495f6601ef8b78ee5a834fd8fa118274d6
Miguel Cid <miguel.cid.paz@gmail.com>
2022-05-12 21:07:51 +0200

    add new endPoint for collect random places

--- controllers/places/getRandoms.js
+++ controllers/places/getRandoms.js
@@ -1,9 +1,7 @@
 const PlaceManager = require('../../managers/PlaceManager')
 
 async function getRandoms(req, res){
-
     const response = await PlaceManager.getRandoms();
-
     res.status(200).json(response.length > 0 ? response : [])
 }
 
