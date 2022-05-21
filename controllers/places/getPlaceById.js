f32c6aece84732a315a030faaebc0091fc66e824
Sebastian M <ra3gan1991@gmail.com>
2022-05-21 02:22:12 +0200

    fix itine

--- controllers/places/getPlaceById.js
+++ controllers/places/getPlaceById.js
@@ -1,7 +1,7 @@
 const PlaceManager = require("../../managers/PlaceManager");
 
 async function getPlaceById(req, res) {
-
+  
   res.status(200).json(await PlaceManager.getPlaceById(req.params.id));
 }
 
